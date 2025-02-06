a6bb84798f21ae25f9399e1cc720539e1c355bbd
Victo-2343383 <2343383@carrefour.cegepvicto.ca>
2025-02-06 10:32:55 -0500

    commentaires RessourceLoader.cpp

--- Voisins/RessourceLoader.cpp
+++ Voisins/RessourceLoader.cpp
@@ -3,8 +3,10 @@
 #include <sstream>
 #include <iostream>
 
-RessourceLoader::RessourceLoader(const string& file_path, bool load_on_init) : file_path(file_path) 
+RessourceLoader::RessourceLoader(const string& file_path, bool load_on_init = false) : file_path(file_path) 
 {
+
+	//seulement initier les données à la déclaration seulement si nécessaire
 	if (load_on_init)
 	{
 		loadData();
@@ -13,6 +15,7 @@ RessourceLoader::RessourceLoader(const string& file_path, bool load_on_init) : f
 
 void RessourceLoader::loadData()
 {
+	//ouvrir le fichier et initier une ligne vide
 	ifstream file(file_path);
 	string line{ "" };
 
@@ -22,12 +25,17 @@ void RessourceLoader::loadData()
 		istringstream s(line);
 		float cell;
 
-		while (s >> cell)
+		while (s >> cell) //tant qu'on peut extraire des floats du stringstream
 		{
+			//ajouter le float au vecteur
 			row.push_back(cell);
+
+			//ignorer la virgule si elle est le prochain char
 			if (s.peek() == ',')
 				s.ignore();
 		}
+
+		//enlever la bool et ajouter le vecteur au data
 		row.pop_back();
 		data.push_back(row);	
 	}
@@ -37,6 +45,7 @@ const vector<vector<float>>& RessourceLoader::getData() const
 {
 	if (data.empty())
 	{
+		//call loadData() en const et initialiser la data
 		const_cast<RessourceLoader*>(this)->loadData();
 	}
 	return data;
@@ -44,25 +53,31 @@ const vector<vector<float>>& RessourceLoader::getData() const
 
 Noeud* RessourceLoader::GetTrainData(double k, vector<vector<float>> donnees)
 {
-	int nbEnreg = donnees.size() * k;
+	//faire une chaine de noeud et retourner le début
+
+	int nbEnreg = donnees.size() * k; // end = size * k
 	Noeud* head = nullptr;
 	Noeud* current = nullptr;
 
 	for (int i{ 0 }; i < nbEnreg; i++)
 	{
+		//...
 		Noeud* nouveau = new Noeud();
 		nouveau->data = donnees[i];
 		nouveau->next = nullptr;
 
-		if (head == nullptr)
+		if (head == nullptr) //exécuter au tout début
 		{
 			head = nouveau;
 			current = nouveau;
 		}
-		else
+		else //le reste du temps:
 		{
 			current->next = nouveau;
 			current = nouveau;
+			
+
+			//on ne change pas head pour garder une trace du début
 		}
 	}
 
@@ -71,27 +86,32 @@ Noeud* RessourceLoader::GetTrainData(double k, vector<vector<float>> donnees)
 
 Noeud* RessourceLoader::GetTestData(double k, vector<vector<float>> donnees)
 {	
+	//faire une chaine de noeud et retourner le début
+
 	int start{ 0 };
-	start = k * donnees.size();
+	start = k * donnees.size(); //start = k-1% = k * size
 
 	Noeud* head = nullptr;
 	Noeud* current = nullptr;
 
 	for (int i{ start }; i < donnees.size(); i++)
 	{
+		//...
 		Noeud* nouveau = new Noeud;
 		nouveau->data = donnees[i];
 		nouveau->next = nullptr;
 
-		if (head == nullptr)
+		if (head == nullptr) //exécuter la première fois
 		{
 			head = nouveau;
 			current = nouveau;
 		}
-		else
+		else //le reste du temps
 		{
 			current->next = nouveau;
 			current = nouveau;
+
+			//on ne change pas head pour garder une trace du début
 		}
 	}
 
